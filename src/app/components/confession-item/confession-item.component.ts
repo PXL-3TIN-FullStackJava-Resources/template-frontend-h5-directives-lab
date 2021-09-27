979f421f42acefd1ebcfdd9083c9a657f3b913d3
Dries Swinnen <dries.swinnen@pxl.be>
2021-09-27 20:15:14 +0200

    initial commit

---
+++ src/app/components/confession-item/confession-item.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Confession } from 'src/app/models/confession.model';
+
+@Component({
+  selector: 'app-confession-item',
+  templateUrl: './confession-item.component.html',
+  styleUrls: ['./confession-item.component.css']
+})
+export class ConfessionItemComponent implements OnInit {
+  @Input() confession!: Confession;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  getDepartmentUrl(department: string): string {
+    department = department.toLowerCase();
+    switch(department) {
+      case 'pxl-digital': {
+         return 'assets/pxl-digital.png'
+      }
+      case 'pxl-mad': {
+        return 'assets/pxl-mad.png'
+      }
+      case 'pxl-business': {
+        return 'assets/pxl-business.png'
+      }
+      case 'pxl-education': {
+        return 'assets/pxl-education.png'
+      }
+      default: {
+        return 'assets/hogeschoolpxl.png'
+      }
+   }
+  }
+
+}
